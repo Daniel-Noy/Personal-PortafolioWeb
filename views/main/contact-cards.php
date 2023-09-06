8ac8cc690b7044db075f54ed5f6c38972aff903a
Daniel Noyola <pepenoyola13@gmail.com>
2023-09-06 17:43:02 -0600

    Ajustes en las variables sass

--- views/main/contact-cards.php
+++ views/main/contact-cards.php
@@ -2,19 +2,19 @@
     <div class="card card--contact">
         <div class="card__info">
             <!-- <h4 class="card__title">Mi correo</h4> -->
-            <div class="card__description"><i class='bx bxs-envelope'></i> danielnm1325@gmail.com</div>
+            <div class="card__description"><i class='bx bxs-envelope || card__icon'></i> danielnm1325@gmail.com</div>
         </div>
     </div>
     <div class="card card--contact">
         <div class="card__info">
             <!-- <h4 class="card__title">Mi correo</h4> -->
-            <div class="card__description"><i class='bx bxs-phone'></i> (+52) 5586885536</div>
+            <div class="card__description"><i class='bx bxs-phone || card__icon'></i> (+52) 5586885536</div>
         </div>
     </div>
     <div class="card card--contact">
         <div class="card__info">
             <!-- <h4 class="card__title">Mi correo</h4> -->
-            <div class="card__description"><i class='bx bxs-map'></i> Estado de México, México</div>
+            <div class="card__description"><i class='bx bxs-map  || card__icon'></i> Estado de México, México</div>
         </div>
     </div>
 </div>
