7dde3ebe26f9e085520db37a1b8f50167a37d28f
Daniel Noyola <pepenoyola13@gmail.com>
2023-09-21 14:38:55 -0600

    Agregado link al repositorio

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,7 +1,8 @@
 <footer class="footer">
     <div class="footer__atributions">
-        <p>Web illustrations by <a href="https://storyset.com/web">Storyset</a></p>
-        <p>Icons by <a href="https://boxicons.com">Box icons</a></p>
+        <p>Web illustrations by <a target="_blank" rel="noopener noreferrer" href="https://storyset.com/web">Storyset</a></p>
+        <p>Icons by <a target="_blank" rel="noopener noreferrer" href="https://boxicons.com">Box icons</a></p>
+        <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/Daniel-Noy/Personal-PortafolioWeb">Github</a> del portafolio</p>
     </div>
     <p class="footer__text">Daniel Noyola Todos los derechos reservados <?php echo date('Y'); ?>&copy;</p>
 </footer>
